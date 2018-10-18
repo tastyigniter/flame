16535a822d3e17b5b0bba4c9f0a43f0f41e8dea9
SamPoyigi <sam@sampoyigi.com>
2018-10-18 18:03:37 +0100

    Include exception trace string in detailed debug message

--- src/Exception/ErrorHandler.php
+++ src/Exception/ErrorHandler.php
@@ -2,6 +2,8 @@
 
 namespace Igniter\Flame\Exception;
 
+use Config;
+use Exception;
 use October\Rain\Exception\ErrorHandler as OctoberErrorHandler;
 
 /**
@@ -9,4 +11,26 @@ use October\Rain\Exception\ErrorHandler as OctoberErrorHandler;
  */
 class ErrorHandler extends OctoberErrorHandler
 {
+    /**
+     * Returns a more descriptive error message if application
+     * debug mode is turned on.
+     * @param Exception $exception
+     * @return string
+     */
+    public static function getDetailedMessage($exception)
+    {
+        $message = $exception->getMessage();
+
+        if (!($exception instanceof ApplicationException) && Config::get('app.debug', FALSE)) {
+            $message = sprintf('"%s" on line %s of %s',
+                $exception->getMessage(),
+                $exception->getLine(),
+                $exception->getFile()
+            );
+
+            $message .= $exception->getTraceAsString();
+        }
+
+        return $message;
+    }
 }
