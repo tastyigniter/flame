84a7ecf8d05d5be6dcc1a8a2b4f4907f21e97b48
SamPoyigi <sam@sampoyigi.com>
2018-12-22 13:45:37 +0000

    Get the validation translations from system/lang
    Signed-off-by: SamPoyigi <sam@sampoyigi.com>

--- src/Translation/Translator.php
+++ src/Translation/Translator.php
@@ -7,10 +7,35 @@ class Translator extends BaseTranslator
 {
     public function get($key, array $replace = [], $locale = null, $fallback = TRUE)
     {
-        // @todo: remove later
         if (Str::startsWith($key, 'lang:'))
             $key = substr($key, 5);
 
+        if ($line = $this->getValidationKey($key, $replace, $locale))
+            return $line;
+
         return parent::get($key, $replace, $locale, $fallback);
+    }
+
+    /**
+     * Get the validation translation.
+     *
+     * @param  string $key
+     * @param  array $replace
+     * @param  string $locale
+     * @return string
+     */
+    protected function getValidationKey($key, $replace, $locale)
+    {
+        if (
+            starts_with($key, 'validation.')
+            AND !starts_with($key, 'validation.custom.')
+            AND !starts_with($key, 'validation.attributes.')
+        ) {
+            $systemKey = 'system::'.$key;
+            $line = $this->get($systemKey, $replace, $locale);
+            if ($line !== $systemKey) {
+                return $line;
+            }
+        }
     }
 }
