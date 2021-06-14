725822e8cc903f2a9fb3ae7e441dfc7af5e57f91
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-06-14 11:49:52 +0100

    Revert location order time restriction model option #33

--- src/Location/AbstractOrderType.php
+++ src/Location/AbstractOrderType.php
@@ -78,6 +78,6 @@ abstract class AbstractOrderType implements OrderTypeInterface
         if ($this->model->getOption('limit_orders'))
             return static::LATER_ONLY;
 
-        return $this->model->getOrderTimeRestriction($this->code);
+        return 0;
     }
 }
