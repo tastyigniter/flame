0c9a42ef31f293b540f9e10c42b055d9217ad778
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-09-06 09:43:09 +0100

    Add laravel 8 mail.manager
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

---
+++ src/Mail/MailManager.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Igniter\Flame\Mail;
+
+use Illuminate\Mail\MailManager as IlluminateMailManager;
+use InvalidArgumentException;
+
+class MailManager extends IlluminateMailManager
+{
+    /**
+     * Resolve the given mailer.
+     *
+     * @param string $name
+     * @return \Illuminate\Mail\Mailer
+     *
+     * @throws \InvalidArgumentException
+     */
+    protected function resolve($name)
+    {
+        $config = $this->getConfig($name);
+
+        if (is_null($config)) {
+            throw new InvalidArgumentException("Mailer [{$name}] is not defined.");
+        }
+
+        // Once we have created the mailer instance we will set a container instance
+        // on the mailer. This allows us to resolve mailer classes via containers
+        // for maximum testability on said classes instead of passing Closures.
+        $mailer = new Mailer(
+            $name,
+            $this->app['view'],
+            $this->createSwiftMailer($config),
+            $this->app['events']
+        );
+
+        if ($this->app->bound('queue')) {
+            $mailer->setQueue($this->app['queue']);
+        }
+
+        // Next we will set all of the global addresses on this mailer, which allows
+        // for easy unification of all "from" addresses as well as easy debugging
+        // of sent messages since these will be sent to a single email address.
+        foreach (['from', 'reply_to', 'to', 'return_path'] as $type) {
+            $this->setGlobalAddress($mailer, $config, $type);
+        }
+
+        return $mailer;
+    }
+}
--- src/Mail/MailServiceProvider.php
+++ src/Mail/MailServiceProvider.php
@@ -8,29 +8,14 @@ class MailServiceProvider extends BaseMailServiceProvider
 {
     protected function registerIlluminateMailer()
     {
-        $this->app->singleton('mailer', function ($app) {
+        $this->app->singleton('mail.manager', function ($app) {
+            return new MailManager($app);
+        });
 
+        $this->app->singleton('mailer', function ($app) {
             $this->app['events']->fire('mailer.beforeRegister', [$this]);
 
-            $config = $app->make('config')->get('mail');
-
-            // Once we have create the mailer instance, we will set a container instance
-            // on the mailer. This allows us to resolve mailer classes via containers
-            // for maximum testability on said classes instead of passing Closures.
-            $mailer = new Mailer(
-                $app['view'], $app['swift.mailer'], $app['events']
-            );
-
-            if ($app->bound('queue')) {
-                $mailer->setQueue($app['queue']);
-            }
-
-            // Next we will set all of the global addresses on this mailer, which allows
-            // for easy unification of all "from" addresses as well as easy debugging
-            // of sent messages since they get be sent into a single email address.
-            foreach (['from', 'reply_to', 'to'] as $type) {
-                $this->setGlobalAddress($mailer, $config, $type);
-            }
+            $mailer = $app->make('mail.manager')->mailer();
 
             $this->app['events']->fire('mailer.register', [$this, $mailer]);
 
