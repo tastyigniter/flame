975d84e8d1f0369ac9991bedc02edffc4d3e96ca
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-05-30 15:54:01 +0100

    wip
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

---
+++ src/System/Database/Migrations/2018_10_21_131044_create_sessions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace System\Database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->text('payload')->nullable();
+            $table->integer('last_activity')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
