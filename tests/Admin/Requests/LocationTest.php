5e5b22ec59825d8c56c855f297953ec63af76a46
Ryan Mitchell <ryan@thoughtcollective.com>
2022-06-21 20:10:44 +0100

    Fix LocationTest failure

--- tests/Admin/Requests/LocationTest.php
+++ tests/Admin/Requests/LocationTest.php
@@ -5,43 +5,49 @@ namespace Tests\Admin\Requests;
 use Igniter\Admin\Requests\Location;
 
 it('has required rule for location_name, location_email and ...', function () {
-    expect('required')->toBeIn(array_get((new Location)->rules(), 'location_name'));
+    $rules = (new Location)->rules();
 
-    expect('required')->toBeIn(array_get((new Location)->rules(), 'location_email'));
+    expect('required')->toBeIn(array_get($rules, 'location_name'));
 
-    expect('required')->toBeIn(array_get((new Location)->rules(), 'location_address_1'));
+    expect('required')->toBeIn(array_get($rules, 'location_email'));
 
-    expect('required')->toBeIn(array_get((new Location)->rules(), 'location_country_id'));
+    expect('required')->toBeIn(array_get($rules, 'location_address_1'));
 
-    expect('required')->toBeIn(array_get((new Location)->rules(), 'options.auto_lat_lng'));
+    expect('required')->toBeIn(array_get($rules, 'location_country_id'));
+
+    expect('required')->toBeIn(array_get($rules, 'options.auto_lat_lng'));
 });
 
 it('has sometimes rule for inputs', function () {
-    expect('sometimes')->toBeIn(array_get((new Location)->rules(), 'location_telephone'));
+    $rules = (new Location)->rules();
 
-    expect('sometimes')->toBeIn(array_get((new Location)->rules(), 'location_lat'));
+    expect('sometimes')->toBeIn(array_get($rules, 'location_telephone'));
 
-    expect('sometimes')->toBeIn(array_get((new Location)->rules(), 'location_lng'));
+    expect('sometimes')->toBeIn(array_get($rules, 'location_lat'));
+
+    expect('sometimes')->toBeIn(array_get($rules, 'location_lng'));
 });
 
 it('has max characters rule for inputs', function () {
-    expect('max:96')->toBeIn(array_get((new Location)->rules(), 'location_email'));
+    $rules = (new Location)->rules();
 
-    expect('between:2,128')->toBeIn(array_get((new Location)->rules(), 'location_address_1'));
+    expect('max:96')->toBeIn(array_get($rules, 'location_email'));
 
-    expect('max:128')->toBeIn(array_get((new Location)->rules(), 'location_address_2'));
+    expect('between:2,128')->toBeIn(array_get($rules, 'location_address_1'));
 
-    expect('max:128')->toBeIn(array_get((new Location)->rules(), 'location_city'));
+    expect('max:128')->toBeIn(array_get($rules, 'location_address_2'));
 
-    expect('max:128')->toBeIn(array_get((new Location)->rules(), 'location_state'));
+    expect('max:128')->toBeIn(array_get($rules, 'location_city'));
 
-    expect('max:15')->toBeIn(array_get((new Location)->rules(), 'location_postcode'));
+    expect('max:128')->toBeIn(array_get($rules, 'location_state'));
 
-    expect('max:3028')->toBeIn(array_get((new Location)->rules(), 'description'));
+    expect('max:15')->toBeIn(array_get($rules, 'location_postcode'));
 
-    expect('max:255')->toBeIn(array_get((new Location)->rules(), 'permalink_slug'));
+    expect('max:3028')->toBeIn(array_get($rules, 'description'));
 
-    expect('max:128')->toBeIn(array_get((new Location)->rules(), 'gallery.title'));
+    expect('max:255')->toBeIn(array_get($rules, 'permalink_slug'));
 
-    expect('max:255')->toBeIn(array_get((new Location)->rules(), 'gallery.description'));
+    expect('max:128')->toBeIn(array_get($rules, 'options.gallery.title'));
+
+    expect('max:255')->toBeIn(array_get($rules, 'options.gallery.description'));
 });
